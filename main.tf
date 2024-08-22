84041d2e6f177b00bc0678fbdf1974dc3a31a8a9
vikash <subhashchandrayadav698@gmail.com>
2024-08-22 14:06:41 +0530

    add files

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["*al2023-ami-2023.3.*-kernel-6.1-x86_64*"]
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
 
   filter {
